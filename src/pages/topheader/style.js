1191f265da9b4c5fb91228c40c088721c8b3998e
shadowstream <nanqingl@qq.com>
2018-12-29 11:09:28 +0800

    header change

---
+++ src/pages/topheader/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+
+export const HeaderWrapper=styled.div`
+    width:620px;
+    overflow:hidden;
+    margin: 0 auto;
+    padding-bottom:100px;
+`
--- src/pages/topheader/topheader.js
+++ src/pages/topheader/topheader.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-
+import { HeaderWrapper, Header, Content } from "./style";
 class TopHeader extends Component {
   render () {
     return (
-      <div>
+      <HeaderWrapper>
         Top Header
-      </div>
+      </HeaderWrapper>
     )
   }
 }
